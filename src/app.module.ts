918f25c55baef8c3d3304910741b920335483a45
Ivisonj <ivison_j@outlook.com>
2024-06-05 14:29:14 -0300

    chore: create module, controller e service auth

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaService } from './shared/infra/database/prisma/prisma-service.module';
 import { UserModule } from './modules/user/user.module';
+import { AuthModule } from './modules/user/use-cases/auth/auth.module';
 
 @Module({
-  imports: [UserModule],
+  imports: [UserModule, AuthModule],
   controllers: [AppController],
   providers: [AppService, PrismaService],
 })
