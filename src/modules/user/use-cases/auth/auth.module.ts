9ebda603dfd5e641296e96b807d2a4e6a27e5953
Ivison Joel <115274427+Ivisonj@users.noreply.github.com>
2024-06-05 23:20:31 -0300

    feat: login implementation

--- src/modules/user/use-cases/auth/auth.module.ts
+++ src/modules/user/use-cases/auth/auth.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
-import { AuthController } from './auth.controller';
+import { JwtModule } from '@nestjs/jwt';
 import { AuthService } from './auth.service';
+import { AuthController } from './auth.controller';
+import { PrismaService } from 'src/shared/infra/database/prisma/prisma-service.module';
 
 @Module({
+  imports: [
+    JwtModule.register({
+      global: true,
+      secret: 'authSecret',
+      signOptions: { expiresIn: '60s' },
+    }),
+  ],
+  providers: [AuthService, PrismaService],
   controllers: [AuthController],
-  providers: [AuthService],
+  exports: [AuthService],
 })
 export class AuthModule {}
